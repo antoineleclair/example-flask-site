6cff62674d81bfcb149d5c9fd83195c49efbb216
Antoine Leclair <antoineleclair@gmail.com>
2025-10-09 17:45:55 -0400

    .

--- server.py
+++ server.py
@@ -11,7 +11,22 @@ proc_id = uuid.uuid4().hex
 @app.route("/")
 def hello_world():
     print("new web request")
-    return f"""<p>hello from disco7!!!
+    return f"""<p>hello from disco7!!!!!!! !!!!!
+    !!!             
+        !!
+        !!
+        !!
+        !!
+        !!
+        asdasdasdas
+        dasdasd
+        !!!
+        ASDASDASD
+        ASDasd
+        ASDASDAS
+        !!!!
+        ASDASDAS
+        !!!!
                <br>the datetime is {datetime.now()}
                <br>the commit is {os.getenv("DISCO_COMMIT")}
                <br>the deployment number is {os.getenv("DISCO_DEPLOYMENT_NUMBER")}
@@ -25,4 +40,6 @@ def health():
 
 
 if __name__ == "__main__":
+    import time
+    time.sleep(60)
     app.run(host="0.0.0.0", port=8080, debug=True)
